d1387543824aba365967b02adf84e2abec55e267
StefanTodoran <stefan.alex4@gmail.com>
2023-01-13 16:19:32 -0800

    first test of template

---
+++ book/chapters/util.py
@@ -0,0 +1,9 @@
+'''
+This script includes all packages and functions required by book chapters
+'''
+# evaluate random forest algorithm for classification
+import numpy as np
+import pandas as pd
+
+def dummy_method():
+    pass
